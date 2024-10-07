11fc4b780034a7cdd2241f72977bb9653026c9f8
frixaco <rustam21ashurmatov@gmail.com>
2024-10-07 21:10:49 +0500

    Add shortcuts toggle button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       boxShadow: {
-        solid: "2px 2px 0",
+        solid: '2px 2px 0'
       },
       dropShadow: {
-        "solid": "2px 2px 0px hsl(0, 0%, 0%)"
+        solid: '2px 2px 0px hsl(0, 0%, 0%)'
       },
       colors: {
-        "primary": "hsl(0deg 0% 9.02%)"
+        primary: 'hsl(0deg 0% 9.02%)'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
