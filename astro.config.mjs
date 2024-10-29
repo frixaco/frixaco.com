7516984e1362f73a98f0c711373ac4db4c6459d9
frixaco <rustam21ashurmatov@gmail.com>
2024-10-29 17:05:56 +0500

    Reduce bundle size (removed Solid)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-import solidJs from "@astrojs/solid-js"
-
 import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), solidJs({ devtools: true }), mdx()],
+	integrations: [tailwind(), mdx()],
 })
