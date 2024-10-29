5f9f66de1ea52cd55cc74414de99b8f007a0b781
frixaco <rustam21ashurmatov@gmail.com>
2024-10-29 21:12:25 +0500

    Enable Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwind from "@astrojs/tailwind"
 
 import mdx from "@astrojs/mdx"
 
+import vercel from "@astrojs/vercel/serverless"
+
 // https://astro.build/config
 export default defineConfig({
+	output: "server",
 	integrations: [tailwind(), mdx()],
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 })
