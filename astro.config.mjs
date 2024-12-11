1171ac5da2f1f5f3596907d99039c9a1f57aaa83
frixaco <rustam21ashurmatov@gmail.com>
2024-12-11 16:41:42 +0500

    Update all packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from "@astrojs/tailwind"
 
 import mdx from "@astrojs/mdx"
 
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
+	output: "static",
 	integrations: [tailwind(), mdx()],
 	adapter: vercel({
 		webAnalytics: { enabled: true },
