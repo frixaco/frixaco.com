6b09e0e4b60e03dafa29a1a6968d704ffcebb30c
frixaco <rustam21ashurmatov@gmail.com>
2024-12-24 11:40:15 +0500

    Fix incorrect activity date parsing

--- src/scripts/activity.ts
+++ src/scripts/activity.ts
@@ -3,63 +3,61 @@ const getActivityMessage = async () => {
 	const data: { platform: string; title: string; url: string; date: string } = await response.json()
 
 	const utcDateString = data.date
-	const utcDate = new Date(utcDateString + "UTC")
+	const utcDate = new Date(utcDateString)
 	const localDate = new Date()
 	const diffInMs = Math.abs(utcDate.getTime() - localDate.getTime())
 	const diffInMinutes = diffInMs / 60000
 
-	let initialActivityMessage = ""
-
 	if (diffInMinutes > 30) {
-		initialActivityMessage = "I'm either AFK, on my phone or doing something else"
+		return "I'm either AFK, on my phone or doing something else"
 	}
 
 	if (data.platform === "browser") {
 		if (["guidetojapanese.org", "animelon.com"].some((domain) => data.url.includes(domain))) {
-			initialActivityMessage = "I'm learning Japanese right now..."
+			return "I'm learning Japanese right now..."
 		}
 
 		if (["youtube.com"].some((domain) => data.url.includes(domain))) {
 			if (data.url.includes("list=PLcjQUNxFpwsRJ6a2vO5EeHMkSd4ck7IbS")) {
-				initialActivityMessage = "I'm listening to my Music playlist right now..."
+				return "I'm listening to my Music playlist right now..."
 			} else {
-				initialActivityMessage = "I'm watching YouTube right now..."
+				return "I'm watching YouTube right now..."
 			}
 		}
 	}
 
 	if (data.platform === "android") {
-		initialActivityMessage = "I'm on my phone right now..."
+		return "I'm on my phone right now..."
 	}
 
 	if (["WezTerm", "wezterm-gui"].includes(data.title)) {
-		initialActivityMessage = "I'm coding right now, using WezTerm..."
+		return "I'm coding right now, using WezTerm..."
 	}
 
 	if (["Cursor", "pwsh"].includes(data.title)) {
-		initialActivityMessage = "I'm coding right now, either working or doing personal projects..."
+		return "I'm coding right now, either working or doing personal projects..."
 	}
 
 	if (["Slack"].includes(data.title)) {
-		initialActivityMessage = "I'm at work right now (in Slack)..."
+		return "I'm at work right now (in Slack)..."
 	}
 
 	if (["Anki", "anki"].includes(data.title)) {
-		initialActivityMessage = "I'm learning Japanese right now (learning new words)..."
+		return "I'm learning Japanese right now (learning new words)..."
 	}
 
 	if (["Heptabase"].includes(data.title)) {
-		initialActivityMessage =
-			"I'm in Heptabase right now. Either doing Japanese or writing/planning something personal..."
+		return "I'm in Heptabase right now. Either doing Japanese or writing/planning something personal..."
 	}
 
 	if (["osu!"].includes(data.title)) {
-		initialActivityMessage = "I'm playing my favorite game, osu! right now..."
+		return "I'm playing my favorite game, osu! right now..."
 	}
 
 	if (["Blender"].includes(data.title)) {
-		initialActivityMessage = "I'm using Blender and doing some 3D modeling right now..."
+		return "I'm using Blender and doing some 3D modeling right now..."
 	}
-	return initialActivityMessage
+
+	return "Tracking not working properly, please let me know if you see this!"
 }
 export { getActivityMessage }
