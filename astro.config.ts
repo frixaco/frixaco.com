03bd88a1f736b88989b9e97de0ef2bf05d913ec8
frixaco <rustam21ashurmatov@gmail.com>
2025-02-19 12:57:24 +0500

    Upgrade all packages

---
+++ astro.config.ts
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config"
+
+import mdx from "@astrojs/mdx"
+
+import vercel from "@astrojs/vercel"
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  integrations: [mdx()],
+
+  adapter: vercel({
+      webAnalytics: { enabled: true },
+	}),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
