f521af02182b3147799ffdefb040a2c373faa28c
frixaco <rustam21ashurmatov@gmail.com>
2025-10-12 17:23:12 +0500

    Minor blog UI change

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -29,7 +29,12 @@ export default async function Page() {
         <article key={i} className="flex relative group text-sm gap-12">
           <div className="flex flex-col flex-1 cursor-pointer gap-1 justify-between">
             <h3 className="line-clamp-1 text-cyber-fg truncate">
-              <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+              <Link
+                href={`/blog/${post.slug}`}
+                className="w-fit group-hover:border-b group-hover:border-b-cyber-grey"
+              >
+                {post.title}
+              </Link>
             </h3>
             <p className="">{post.description}</p>
           </div>
@@ -40,7 +45,7 @@ export default async function Page() {
             <span className="text-xs text-nowrap">12 min read</span>
           </div>
 
-          <div className="absolute top-0 bottom-0 -left-4 w-0.5 scale-y-0 group-hover:scale-y-100 origin-center group-hover:delay-100 transition-transform duration-300 bg-cyber-grey"></div>
+          {/* <div className="absolute top-0 bottom-0 -left-4 w-0.5 scale-y-0 group-hover:scale-y-100 origin-center group-hover:delay-100 transition-transform duration-300 bg-cyber-grey"></div> */}
           {/* <span className="absolute left-0 bottom-0 -translate-x-5/6 -translate-y-1 -rotate-90 text-cyber-grey text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:delay-100"> */}
           {/*   255 */}
           {/* </span> */}
