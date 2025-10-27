2a15af20fae68218d53ece172ed08de3cccabc0f
frixaco <rustam21ashurmatov@gmail.com>
2025-10-27 16:49:04 +0500

    Migrate (again) to Astro (again)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://astro.frixaco.com",
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "IBM Plex Mono",
+        cssVariable: "--font-mono",
+        weights: ["light", "regular", "medium", "semibold", "bold"],
+      },
+    ],
+  },
 });
